69feee62e2d97a1d7db7a8a4bb346a3a37b698f3
liucancan <916215796@qq.com>
2019-04-16 15:29:42 +0800

    no message

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,14 @@ Page({
     // 经度
     long: 0,
     // 维度
-    lat: 0
+    lat: 0,
+    // 控件
+    controls: []
   },
   // 页面初始化时加载
   onLoad: function () {
     var that = this;
+    // 获取设备定位的经纬度
     wx.getLocation({
       success: function (res) {
         var long = res.longitude;
@@ -17,6 +20,20 @@ Page({
           lat: lat
         })
       }
+    })
+    that.setData({
+      controls: [
+        {
+          id: 1,
+          iconPath: "/images/qrcode.png",
+          position: {
+            width: 100,
+            height: 60,
+            left: 100,
+            top: 400
+          }
+        }
+      ]
     })
   }
 })
