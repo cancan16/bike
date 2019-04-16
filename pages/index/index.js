e66f2b33e832698a5f32149e05c18d1edffbeeb5
liucancan <916215796@qq.com>
2019-04-16 17:28:31 +0800

    no message

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,20 @@
 Page({
+  /**
+   * 页面数据
+   */
   data: {
     // 经度
     long: 0,
     // 维度
     lat: 0,
     // 控件
-    controls: []
+    controls: [],
+    // 车辆图标
+    markers: []
   },
-  // 页面初始化时加载
+  /**
+   * 页面初始化时加载
+   */
   onLoad: function () {
     var that = this;
     // 获取设备定位的经纬度
@@ -21,7 +28,9 @@ Page({
         })
       }
     })
-    // 获取设备信息
+    /**
+     * 获取设备信息
+     */
     wx.getSystemInfo({
       success: function (res) {
         console.log(res);
@@ -43,18 +52,6 @@ Page({
               clickable: true
             },
             {
-              id: 3,
-              iconPath: "/images/qrcode.png",
-              position: {
-                width: 100,
-                height: 40,
-                left: windowWidth / 2 - 50,
-                top: windowHeight - 60
-              },
-              //是否可点击
-              clickable: true
-            },
-            {
               //定位按钮安置
               id: 2,
               iconPath: '/images/img1.png',
@@ -63,6 +60,18 @@ Page({
                 height: 40,
                 left: 10,
                 top: windowHeight - 60.
+              },
+              //是否可点击
+              clickable: true
+            },
+            {
+              id: 3,
+              iconPath: "/images/qrcode.png",
+              position: {
+                width: 100,
+                height: 40,
+                left: windowWidth / 2 - 50,
+                top: windowHeight - 60
               },
               //是否可点击
               clickable: true
@@ -92,16 +101,77 @@ Page({
               //是否可点击
               clickable: true
             },
+            { // 添加车辆控件
+              id: 6,
+              iconPath: "/images/add.png",
+              position: {
+                width: 35,
+                height: 35
+              },
+              //是否可点击
+              clickable: true
+            }
           ]
         })
       }
     })
   },
-  // 控件点击时触发事件
+  /**
+   * 控件点击时触发事件
+   */
   controltap: function (e) {
+    var that = this;
     var cid = e.controlId;
-    if (cid == 2) {
-      this.mapCtx.moveToLocation();
+    switch (cid) {
+      // 定位按钮
+      case 2: {
+        this.mapCtx.moveToLocation();
+        break;
+      }
+      // 添加车辆按钮
+      case 6: {
+        // 获取当前已有的车辆
+        var bikes = that.data.markers;
+        bikes.push({
+          iconPath: "/images/bike.png",
+          width: 35,
+          height: 40,
+          longitude: that.data.long,
+          latitude: that.data.lat
+        },
+          {
+            iconPath: "/images/bike.png",
+            width: 35,
+            height: 40,
+            longitude: that.data.long,
+            latitude: that.data.lat
+          });
+        // 赋值
+        that.setData({
+          markers: bikes
+        })
+        break;
+      }
+    }
+  },
+  /**
+   * 中心点坐标移动时触发
+   */
+  regionchange: function (e) {
+    console.log(1);
+    var that = this;
+    var etype = e.type;
+    if (etype == "end") {
+      this.mapCtx.getCenterLocation({
+        success: function (res) {
+          var long = res.longitude;
+          var lat = res.latitude;
+          that.setData({
+            long: long,
+            lat: lat
+          })
+        }
+      })
     }
   },
   /**
