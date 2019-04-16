791b2703ab1fd110d983b9578c3628925e9ac3d8
liucancan <916215796@qq.com>
2019-04-16 15:50:01 +0800

    初始化bike小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,19 +21,80 @@ Page({
         })
       }
     })
-    that.setData({
-      controls: [
-        {
-          id: 1,
-          iconPath: "/images/qrcode.png",
-          position: {
-            width: 100,
-            height: 60,
-            left: 100,
-            top: 400
-          }
-        }
-      ]
+    // 获取设备信息
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res);
+        var windowWidth = res.windowWidth;
+        var windowHeight = res.windowHeight;
+        that.setData({
+          controls: [
+            {
+              //中心点位置
+              id: 1,
+              iconPath: '/images/location.png',
+              position: {
+                width: 20,
+                height: 35,
+                left: windowWidth / 2 - 10,
+                top: windowHeight / 2 - 40.
+              },
+              //是否可点击
+              clickable: true
+            },
+            {
+              id: 3,
+              iconPath: "/images/qrcode.png",
+              position: {
+                width: 100,
+                height: 40,
+                left: windowWidth / 2 - 50,
+                top: windowHeight - 60
+              },
+              //是否可点击
+              clickable: true
+            },
+            {
+              //定位按钮安置
+              id: 2,
+              iconPath: '/images/img1.png',
+              position: {
+                width: 40,
+                height: 40,
+                left: 10,
+                top: windowHeight - 60.
+              },
+              //是否可点击
+              clickable: true
+            },
+            {
+              //定位按钮安置
+              id: 4,
+              iconPath: '/images/pay.png',
+              position: {
+                width: 40,
+                height: 40,
+                left: windowWidth - 45,
+                top: windowHeight - 100.
+              },
+              //是否可点击
+              clickable: true
+            },
+            { //报修
+              id: 5,
+              iconPath: "/images/warn.png",
+              position: {
+                width: 35,
+                height: 35,
+                left: windowWidth - 42,
+                top: windowHeight - 60.
+              },
+              //是否可点击
+              clickable: true
+            },
+          ]
+        })
+      }
     })
   }
 })
